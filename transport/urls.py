d98fca6e7b20bb0cf645309842184d9809baec35
Ondřej Garncarz <ondrej@garncarz.cz>
2015-12-12 06:33:52 +0100

    Google map with Trips.

--- transport/urls.py
+++ transport/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.all_trips_stops, name='all_trips_stops'),
+    url(r'^map/$', views.stops_map, name='map'),
+    url(r'^source/$', views.all_trips_stops, name='all_trips_stops'),
 ]
--- transport/views.py
+++ transport/views.py
@@ -1,11 +1,16 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 
 from . import models
 
 
+def stops_map(request):
+    return render(request, 'transport/map.html')
+
+
 def all_trips_stops(request):
     trips = []
-    for trip in models.Trip.objects.all()[:10]:
+    for trip in models.Trip.objects.all():
         stops = []
         for stop_time in trip.stop_times.all():
             stops.append((stop_time.stop.lat, stop_time.stop.lon))
