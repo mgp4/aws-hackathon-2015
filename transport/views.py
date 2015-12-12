c28815faa05cbb60f634298847f7fe30d03467c9
Ondřej Garncarz <ondrej@garncarz.cz>
2015-12-12 07:37:16 +0100

    #6: Faster retrieval from DB.

--- transport/views.py
+++ transport/views.py
@@ -12,7 +12,7 @@ def all_trips_stops(request):
     trips = []
     for trip in models.Trip.objects.all():
         stops = []
-        for stop_time in trip.stop_times.all():
+        for stop_time in trip.stop_times.all().select_related('stop'):
             stops.append((stop_time.stop.lat, stop_time.stop.lon))
         trips.append(stops)
     return JsonResponse(trips, safe=False)
