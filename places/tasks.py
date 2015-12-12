596befe3784d40e006d4dcbc9291eb95d5458c6b
Jan Richter <richter@kajot.cz>
2015-12-12 14:22:28 +0100

    task to download places data from s3

--- places/tasks.py
+++ places/tasks.py
@@ -8,6 +8,13 @@ from . import models
 
 PLACES = '/tmp/data.json'
 
+def download_places_data():
+    s3 = boto3.resource('s3')
+    s3.meta.client.download_file(
+        settings.AWS_STORAGE_BUCKET_NAME,
+        'data.json',
+        PLACES
+    )
 
 def import_places():
     with open(PLACES, 'r') as data:
