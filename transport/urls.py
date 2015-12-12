4f7539b53e9a4b315e873699312f80a9c6fa9555
Ondřej Garncarz <ondrej@garncarz.cz>
2015-12-12 09:29:03 +0100

    Map's source is relative.

--- transport/urls.py
+++ transport/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^map/$', views.stops_map, name='map'),
-    url(r'^source/$', views.all_trips_stops, name='all_trips_stops'),
+    url(r'^source/$', views.all_trips_stops, name='source'),
 ]
