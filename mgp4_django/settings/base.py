f4548739102352ea6dd340f70cebf0c1bd0fa4d0
Ondřej Garncarz <ondrej@garncarz.cz>
2015-12-12 10:59:26 +0100

    #6: caching lines.

--- mgp4_django/settings/base.py
+++ mgp4_django/settings/base.py
@@ -94,6 +94,17 @@ DATABASES = {
 }
 
 
+# Cache
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        # 'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        'LOCATION': '127.0.0.1:11211',
+    },
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
--- transport/views.py
+++ transport/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -11,8 +12,12 @@ def stops_map(request):
 def all_trips_stops(request):
     trips = []
     for trip in models.Trip.objects.all():
-        stops = []
-        for stop_time in trip.stop_times.all().select_related('stop'):
-            stops.append((stop_time.stop.lat, stop_time.stop.lon))
+        cache_key = 'trip_%d_stops' % trip.id
+        stops = cache.get(cache_key)
+        if not stops:
+            stops = []
+            for stop_time in trip.stop_times.all().select_related('stop'):
+                stops.append((stop_time.stop.lat, stop_time.stop.lon))
+            cache.set(cache_key, stops)
         trips.append(stops)
     return JsonResponse(trips, safe=False)
